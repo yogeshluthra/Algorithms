9039067acb5f6fff7ed779ab0aea6f8ce5b92a11
Yogesh Luthra <y.luthra@samsung.com>
2019-02-19 16:23:40 -0800

    added min heap starter code

---
+++ min_heap.py
@@ -0,0 +1,27 @@
+
+class MinHeap(object):
+    def __init__(self, l = []):
+        self.h = l
+        self._heapify(self.h)
+
+    def _heapify(self):
+        """heapify in-place."""
+        pass
+
+    def push(self, e):
+        """push element e onto heap"""
+        pass
+
+    def pop(self):
+        """pop min element from the heap l"""
+        minE = None
+        # TODO: Do something
+        return minE
+
+    def pushpop(self, e):
+        """push an element e onto heap l and pop min element after that"""
+        minE = None
+        # TODO: Do something
+        return minE
+
+
